4887f98fe4779535a0103b8122b3f0a1eec23c68
Spencer Adams <spencer.adams93@gmail.com>
2019-12-26 20:21:08 -0600

    add data, add single listing

---
+++ add_single_listing.py
@@ -0,0 +1,29 @@
+from os import path
+from random import uniform
+from time import sleep
+from time import time
+
+import pandas as pd
+
+from listing_info import get_all_listing_info
+
+if __name__ == '__main__':
+
+    if path.exists("data/listings/chicago_listings.csv"):
+        listings = pd.read_csv('data/listings/chicago_listings.csv')
+        listing_ids = set(listings['id'].tolist())
+    else:
+        listings = pd.DataFrame()
+        listing_ids = set()
+
+    listing_id = input("Listing ID: ")
+    start = time()
+    print("listing id:", listing_id)
+    listing = get_all_listing_info(listing_id)
+    if listing is not None:
+        listings = pd.concat([listings, listing])
+        listings.to_csv("data/listings/chicago_listings.csv", index=False)
+        sleep_for = uniform(1, 5)
+        sleep(sleep_for)
+        print(f"this listing took: {time() - start} seconds")
+        print("\n")
--- get_all_listings_info.py
+++ get_all_listings_info.py
@@ -1,3 +1,4 @@
+from os import path
 from random import uniform, shuffle
 from time import sleep
 from time import time
@@ -10,18 +11,26 @@ from listing_info import get_all_listing_info
 # TODO add checkin and checkout dates to df?
 
 if __name__ == '__main__':
-    ids = pd.read_csv('chicago_listing_ids.csv')['ids'].tolist()
-    shuffle(ids)
+    ids = set(pd.read_csv('data/ids/chicago_listing_ids.csv')['ids'].tolist())
 
-    df = pd.DataFrame()
+    if path.exists("data/listings/chicago_listings.csv"):
+        listings = pd.read_csv('data/listings/chicago_listings.csv')
+        listing_ids = set(listings['id'].tolist())
+    else:
+        listings = pd.DataFrame()
+        listing_ids = set()
+
+    ids = list(ids - listing_ids)
+
+    shuffle(ids)
 
     for listing_id in ids:
         start = time()
         print("listing id:", listing_id)
         listing = get_all_listing_info(listing_id)
         if listing is not None:
-            df = pd.concat([df, listing])
-            df.to_csv("chicago_listings.csv")
+            listings = pd.concat([listings, listing])
+            listings.to_csv("data/listings/chicago_listings.csv", index=False)
             sleep_for = uniform(1, 5)
             sleep(sleep_for)
             print(f"this listing took: {time() - start} seconds")
